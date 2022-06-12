e788d4ab7f421a4fcf29c4be1b48643632ec04ac
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1 <acwell94@naver.com>
2022-06-12 17:45:27 +0900

    추가 : Modal 추가중

--- src/components/commons/layout/banner/Banner.styles.ts
+++ src/components/commons/layout/banner/Banner.styles.ts
@@ -2,6 +2,6 @@ import styled from "@emotion/styled";
 
 export const BannerWrapper = styled.div`
   width: 100%;
-  height: 400px;
-  display: inline;
+  display: flex;
+  justify-content: center;
 `;
